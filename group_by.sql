d89f56507fda83134cabe859d77c169c2b68fc98
Tate Frost <tatefrost96@gmail.com>
2021-12-06 16:07:38 -0700

    group completed

---
+++ group_by.sql
@@ -0,0 +1,11 @@
+-- problem 1
+SELECT sum(total) FROM invoice 
+GROUP BY billing_state;
+
+-- problem 2
+SELECT AVG(milliseconds) FROM track 
+GROUP BY album_id ORDER BY AVG;
+
+-- problem 3
+SELECT count(*) FROM album WHERE artist_id IN (8, 22) GROUP BY artist_id
+
