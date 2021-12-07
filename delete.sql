1e732d64713d118f2565f9d06daa310497cb149e
Tate Frost <tatefrost96@gmail.com>
2021-12-06 20:11:27 -0700

    delete complete

--- delete.sql
+++ delete.sql
@@ -1,3 +1,35 @@
+-- problem 1
+CREATE TABLE animals (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(20),
+  type VARCHAR(20),
+  age INTEGER
+);
+
+-- problem 2
+INSERT INTO animals ( name, type, age ) 
+VALUES ('Leo', 'lion', 12),
+('Jerry', 'mouse', 4),
+('Marty', 'zebra', 10),
+('Gloria', 'hippo', 8),
+('Alex', 'lion', 9),
+('Melman', 'giraffe', 15),
+('Nala', 'lion', 2),
+('Marie', 'cat', 1),
+('Flounder', 'fish', 8);
+
+-- problem 3
+SELECT * FROM animals
+
+-- problem 4
+DELETE FROM animals WHERE type = 'lion';
+
+-- problem 5
+DELETE FROM animals WHERE name LIKE 'M%';
+
+-- problem 6
+DELETE FROM animals WHERE age < 9;
+
 -- INSERT INTO animals ( name, type, age ) 
 -- VALUES ('Leo', 'lion', 12),
 -- ('Jerry', 'mouse', 4),
@@ -8,3 +40,4 @@
 -- ('Nala', 'lion', 2),
 -- ('Marie', 'cat', 1),
 -- ('Flounder', 'fish', 8);
+
